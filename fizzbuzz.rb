978faba6215080f7ac0ecfc01992865f6332d7ff
Jake Garcia <bballwiz4@gmail.com>
2020-02-18 17:09:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 def fizzbuzz(num)
 
   if num % 3 == 0 && num % 5 == 0
-    return "fizzbuzz"
+    return "FizzBuzz"
   elsif num % 3 == 0
-    return "fizz"
+    return "Fizz"
   elsif num % 5 == 0
-    return "buzz"
+    return "Buzz"
   end
   end
 
